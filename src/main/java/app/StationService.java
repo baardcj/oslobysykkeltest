696d5d90665b18557b52b9e7619053a96b04367c
baardcj <baardcj@outlook.com>
2020-05-02 17:22:18 +0200

    added simple error catching/logging

--- src/main/java/app/StationService.java
+++ src/main/java/app/StationService.java
@@ -4,11 +4,16 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import javax.annotation.PostConstruct;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClientResponseException;
 import org.springframework.web.client.RestTemplate;
 
 import app.model.Station;
@@ -19,56 +24,79 @@ import app.modelwrappers.WrapDataStationStatus;
 @EnableScheduling
 @Service
 public class StationService {
+	
+	private static final Logger log = LoggerFactory.getLogger(StationService.class);
 
 	private final String stationInfoURL = "https://gbfs.urbansharing.com/oslobysykkel.no/station_information.json";
 	private final String stationStatusURL = "https://gbfs.urbansharing.com/oslobysykkel.no/station_status.json";
     
-	private Map<Integer, Station> stationMap = new HashMap<Integer, Station>();
-	private RestTemplate restTemplate = new RestTemplate();
+	private final Map<Integer, Station> stationMap = new HashMap<Integer, Station>();
+	private final RestTemplate restTemplate = new RestTemplate();
 
-	
+
     @Bean
     public Map<Integer, Station> stationMap(){
     	return stationMap;
     }
     
     
-    public void addStation(int id, Station station) {
-    	stationMap.put(id, station);
-    }
-    
-    
-    public Station getStation(int id) {
-    	return stationMap.get(id);
-    }
-        
-    
-    @Scheduled(fixedRate=30000)
+    @Scheduled(fixedRate=60000)
     private void updateStationStatus() {
-    	System.out.println("In service.class updateStationStatus()"); 
-    	ResponseEntity<WrapDataStationStatus> statStatus = restTemplate.getForEntity(stationStatusURL, WrapDataStationStatus.class);
-		List<StationStatus> statStatusList = statStatus.getBody().getData().getStations();
-		
-		for(StationStatus stStat : statStatusList) {
-			Station st = getStation(stStat.getStation_id());
-			st.setNum_bikes_available(stStat.getNum_bikes_available());
-			st.setNum_docks_available(stStat.getNum_docks_available());
-		}
+    	
+    	if(stationMap.size() != 0){
+    		log.info("UPDATING (station status)");
+    	
+    		try{
+    			ResponseEntity<WrapDataStationStatus> statStatus = restTemplate.getForEntity(stationStatusURL, WrapDataStationStatus.class);
+    			log.info(statStatus.getStatusCode().toString() + " (updateStationStatus)" ); 
+    			
+    			try {
+    	    		List<StationStatus> statStatusList = statStatus.getBody().getData().getStations();
+    	    		
+    	    		for(StationStatus stStat : statStatusList) {
+    	    			Station st = stationMap.get(stStat.getStation_id());
+    	    			st.setNum_bikes_available(stStat.getNum_bikes_available());
+    	    			st.setNum_docks_available(stStat.getNum_docks_available());
+    	    		}
+    				
+            	} catch (NullPointerException e){
+            		log.error("NullPointerException");
+            	}
+    		
+    		} catch (RestClientResponseException e) {     	
+            	log.error("RestClientResponseException");
+            }
+    	} else {
+    		log.warn("NOT UPDATING (empty stationMap)");
+    	}
     }
 	
     
     @PostConstruct
     public void init() {
-        System.out.println("In service.class init()"); 
-
-		ResponseEntity<WrapDataStationInfo> statInfo = restTemplate.getForEntity(stationInfoURL, WrapDataStationInfo.class);
-		List<Station> statInfoList = statInfo.getBody().getData().getStations();
+    	
+        log.info("INITIALISING");
+        
+        try {
+        	ResponseEntity<WrapDataStationInfo> statInfo = restTemplate.getForEntity(stationInfoURL, WrapDataStationInfo.class);
+        	log.info(statInfo.getStatusCode().toString() + " (init)" ); 
+        	
+        	try {
+        		List<Station> statInfoList = statInfo.getBody().getData().getStations();
 		
-		for(Station st : statInfoList) {
-			addStation(st.getStation_id(), st);
-		}
+        		for(Station st : statInfoList) {
+        			stationMap.put(st.getStation_id(), st);
+        		}
+   
+        		updateStationStatus();
+        		
+        	} catch (NullPointerException e){
+        		log.error("NullPointerException");
+        	}
+        } catch (RestClientResponseException e) {     	
+        	log.error("RestClientResponseException");
+        }
 		
-		updateStationStatus();
     }
 	
 }
